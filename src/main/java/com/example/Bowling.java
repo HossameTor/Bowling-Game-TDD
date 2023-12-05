c54b49bf141c8cd0a4810c7c653fbafc17648cea
HossameTor <hossametorchi07@gmail.com>
2023-12-05 03:32:39 +0100

    [Add] Strile

--- src/main/java/com/example/Bowling.java
+++ src/main/java/com/example/Bowling.java
@@ -3,16 +3,35 @@ package com.example;
 public class Bowling {
     private int[] rolls = new int[20];
     private int currentRoll = 0;
+
     public int score(){
-        int score = 0;
-        for(int i = 0 ; i<20 ; i+=2){
-            if(i<=18 && rolls[i+1]+rolls[i] == 10) score+= 10 +rolls[i+2];
-            else score+=rolls[i]+rolls[i+1];
+        int score =0;
+        int firstInFrame = 0;
+        for (int frame = 0; frame < 10; frame++) {
+            if(isStrike(firstInFrame)){
+                score+= 10+rolls[firstInFrame+1]+rolls[firstInFrame+2];
+                firstInFrame +=1;
+            }
+            else if (isSpare(firstInFrame)){
+                score += 10 + rolls[firstInFrame+2];
+                firstInFrame += 2;
+            }else{
+                score+= rolls[firstInFrame]+rolls[firstInFrame+1];
+                firstInFrame += 2;
+            }
         }
         return score;
     }
-    public void roll(int pins){
-        rolls[currentRoll] = pins;
-        currentRoll++;
+
+    public boolean isStrike(int firstInFrame){
+        return rolls[firstInFrame]==10;
+    }
+
+    public boolean isSpare(int firstInFrame){
+        return (rolls[firstInFrame]+rolls[firstInFrame+1]==10);
+    }
+
+    public void roll(int pins) {
+        rolls[currentRoll++] = pins;
     }
 }
--- src/test/java/BowlingTest.java
+++ src/test/java/BowlingTest.java
@@ -1,33 +1,64 @@
-import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import com.example.Bowling;
 
+import junit.framework.Assert;
+
+/**
+ * BowlingTest
+ */
 public class BowlingTest {
-    @Test
-    public void gutterGameTest(){
-        Bowling bowling = new Bowling();
-        for(int i=0; i<20; i++){
-            bowling.roll(0);
+
+    private Bowling bowling;
+
+    private void rollMany(int n, int pins) {
+        for (int index = 0; index < n; index++) {
+            bowling.roll(pins);
         }
+    }
+
+    @Before
+    public void setUp() {
+        this.bowling = new Bowling();
+    }
+
+    @Test
+    public void canCalculateGutterGame() {
+        rollMany(20, 0);
+
         Assert.assertEquals(0, bowling.score());
     }
-    @Test 
-    public void allOnesTest(){
-        Bowling bowling = new Bowling();
-        for(int i=0; i<20; i++){
-            bowling.roll(1);
-        }
+
+    @Test
+    public void canCalculateAllOnes() {
+
+        rollMany(20, 1);
         Assert.assertEquals(20, bowling.score());
     }
-    
-    @Test 
-    public void spareTest(){
-        Bowling bowling = new Bowling();
-        bowling.roll(4); bowling.roll(6); bowling.roll(4); bowling.roll(3);
-        for(int i=0; i<16; i++){
-            bowling.roll(0);
-        }
-        Assert.assertEquals(21, bowling.score());
+
+    @Test
+    public void canCalculateSpare(){
+        bowling.roll(4);
+        bowling.roll(6);
+        bowling.roll(3);
+        bowling.roll(4);
+        rollMany(16,0);
+        Assert.assertEquals(20,bowling.score());
+    }
+
+    @Test
+    public void canCalculateStrike(){
+        bowling.roll(10);
+        bowling.roll(4);
+        bowling.roll(3);
+        rollMany(16,0);
+        Assert.assertEquals(24,bowling.score());
+    }
+
+    @Test
+    public void canCalculatePerfectGame(){
+        rollMany(12,10);
+        Assert.assertEquals(300,bowling.score());
     }
 }
